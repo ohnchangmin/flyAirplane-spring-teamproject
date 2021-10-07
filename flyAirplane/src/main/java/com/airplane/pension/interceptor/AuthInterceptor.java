d8d696a2071d40a43b8fb491bb3eee9b55b12bf7
ohnchangmin <ok_ddol@naver.com>
2021-10-07 22:32:31 +0900

    공지사항 작성, 수정, 삭제 관리자 권한 부여

--- flyAirplane/src/main/java/com/airplane/pension/interceptor/AuthInterceptor.java
+++ flyAirplane/src/main/java/com/airplane/pension/interceptor/AuthInterceptor.java
@@ -8,24 +8,40 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class AuthInterceptor extends HandlerInterceptorAdapter{
+import com.airplane.pension.constant.Role;
+import com.airplane.pension.dto.Member;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception{
+			throws Exception {
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
+		Member roleCheck = (Member) session.getAttribute("member");
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html; charset=utf-8");
 		
-		if(session.getAttribute("adminId") == null) {
-			response.setContentType("text/html; charset=utf-8");
-			PrintWriter out = response.getWriter();
-			String message = "<script>alert('권한이 필요합니다. 로그인 해주세요'); location.href='/pension/admin/adminLoginView';</script>";
+		if (session.getAttribute("member") == null) {
+			String message = "<script>alert('권한이 필요합니다. 로그인 해주세요'); location.href='/pension/member/loginForm';</script>";
 			out.println(message);
 			out.flush();
 			out.close();
 			return false;
 		}
-		return true;
+		else {
+			if(roleCheck.getRole().equals(Role.USER)) {
+				String message = "<script>alert('관리자 권한이 필요합니다.'); location.href='/pension/notice/list';</script>";
+				out.println(message);
+				out.flush();
+				out.close();
+				System.out.println("권한필요해서 튕김" + roleCheck.getRole().equals(Role.USER));
+				return false;
+			}
+			else {
+				System.out.println("어드민이라 넘어감" + roleCheck.getRole().equals(Role.USER));
+				return true;
+			}			
+		}
 	}
-
 }
